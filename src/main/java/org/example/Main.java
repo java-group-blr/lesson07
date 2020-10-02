0b63760de8fbe0a38007c818b58c56a385120318
Siarhei Charepka <sergei1770984@gmail.com>
2020-10-02 22:25:20 +0300

    with logger

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,6 @@
 package org.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 
 public class Main {
@@ -9,8 +8,9 @@ public class Main {
   public static void main(String... args) throws IOException {
     Weather weatherInfo = new Weather();
     weatherInfo.getWeatherInfo();
+
     Logger logger = LoggerFactory.getLogger(Main.class);
-    logger.info("программа завершена успешно");
+    logger.info("Hey!!!");
 
 
   }
